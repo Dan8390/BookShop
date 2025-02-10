dd7e0573d5cb99146d413c88d9cd1a71bb899ffe
Daniel <daniel.byshovets@gmail.com>
2025-02-09 19:58:29 -0800

    Commit 7: almost finished developing employee, still may have some bugs

--- main/employee/urls.py
+++ main/employee/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_employee_menu, name="employee_menu"),
-    path('account', views.show_account, name="account"),
-    path('library', views.show_library, name="library"),
-    path('sales', views.show_sales, name="sales"),
+    path('<int:employee_id>/', views.show_employee_menu, name="employee_menu"),
+    path('<int:employee_id>/account', views.show_account, name="account"),
+    path('<int:employee_id>/library/', views.show_library, name="library"),
+    path('<int:employee_id>/sales', views.show_sales, name="sales"),
     path('login', views.show_login, name="login"),
-    path('create', views.show_create, name="create")
+    path('create', views.show_create, name="create"),
+    path('<int:pk>/employee_update', views.EmployeeUpdateView.as_view(), name="employee_update")
 ]
--- main/employee/views.py
+++ main/employee/views.py
@@ -1,32 +1,58 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Employee
+from .models import Employee, Book, Sale
 from .forms import EmployeeForm
+from django.views.generic import UpdateView
+import hashlib
 
 # Create your views here.
 
 
-def show_employee_menu(request):
-    return render(request, "employee/employee_menu.html")
+class EmployeeUpdateView(UpdateView):
+    model = Employee
+    success_url = "../.."
+    template_name = "employee/employee_update.html"
+    form_class = EmployeeForm
 
 
-def show_account(request):
-    return render(request, "employee/account.html")
+def compute_sha512_hash(input_string):
+    sha512_hash = hashlib.sha512()
+    sha512_hash.update(input_string.encode('utf-8'))
+    return sha512_hash.hexdigest()
 
 
-def show_library(request):
-    return render(request, "employee/library.html")
+def show_employee_menu(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    return render(request, "employee/employee_menu.html", {'employee': employee})
 
 
-def show_sales(request):
-    return render(request, "employee/sales.html")
+def show_account(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    return render(request, "employee/account.html", {'employee': employee})
+
+
+def show_library(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    books = Book.objects.all()
+    return render(request, "employee/library.html", {'books': books, 'employee': employee})
+
+
+def show_sales(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    sales = Sale.objects.all()
+    employee_sales = []
+    for sale in sales:
+        if sale.employee.id == employee_id:
+            employee_sales.append(sale)
+    return render(request, "employee/sales.html", {'employee_sales': employee_sales, 'employee': employee})
 
 
 def show_login(request):
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
+        hash_password = compute_sha512_hash(password)
         try:
-            employee = Employee.objects.get(email=email, password=password)
+            employee = Employee.objects.get(email=email, password=hash_password)
             return redirect("employee_menu", employee_id=employee.id)
         except:
             return render(request, "employee/login.html")
@@ -38,7 +64,9 @@ def show_create(request):
     if request.method == "POST":
         form = EmployeeForm(request.POST)
         if form.is_valid():
-            form.save()
+            employee = form.save(commit=False)
+            employee.password = str(compute_sha512_hash(employee.password))
+            employee.save()
             return redirect("login")
 
     form = EmployeeForm
